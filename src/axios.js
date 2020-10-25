4f820787011fd9568d467a4d2695867887971db9
Harsh <harshc3294@gmail.com>
2020-10-26 00:49:44 +0530

    important

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // THE API (cloud function) URL
+    // baseURL: "https://us-central1-challenge-4b2b2.cloudfunctions.net/api"
     baseURL: 'http://localhost:5001/clone-e8ec7/us-central1/api'
     // baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
 });
