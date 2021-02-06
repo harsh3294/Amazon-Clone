fed8c8ecf1b144ee8dc172bad6921914077c674f
harsh3294 <harshc3294@gmail.com>
2021-02-06 18:44:19 +0530

    deployed hositing

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const cors = require('cors');
-const { request, response } = require('express');
-const stripe = require("stripe")("sk_test_51HgASPKSHXiQDQbdcByV3aCP7FcMuoswucyDoYv2c4SMCgNCq8sJGG1S1dZwbQn4lorAdjbCOojo7rOtr1OwdSHG00x1njdnKG")
-
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { request, response } = require("express");
+const stripe = require("stripe")(
+  "sk_test_51HgASPKSHXiQDQbdcByV3aCP7FcMuoswucyDoYv2c4SMCgNCq8sJGG1S1dZwbQn4lorAdjbCOojo7rOtr1OwdSHG00x1njdnKG"
+);
 // setup api
 
 //App Config
@@ -13,19 +14,18 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 //api routes
-app.get("/", (request, response) => response.status(200).send('hello world'));
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-app.post('/payments/create', async (request, response) =>
-{
-    const total = request.query.total;
-    console.log('payment request recieved ', total);
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total, // sub units of the currency
-        currency: "inr",
-    });
-    response.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    });
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("payment request recieved ", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // sub units of the currency
+    currency: "inr",
+  });
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 //listen commands
 exports.api = functions.https.onRequest(app);
--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // THE API (cloud function) URL
-    // baseURL: "https://us-central1-challenge-4b2b2.cloudfunctions.net/api"
-    baseURL: 'http://localhost:5001/clone-e8ec7/us-central1/api'
-    // baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
+  // THE API (cloud function) URL
+  //   baseURL: "http://localhost:8001",
+  baseURL: "https://cloneamazon.herokuapp.com/",
+  // baseURL: "https://us-central1-challenge-4b2b2.cloudfunctions.net/api"
+  // baseURL: 'http://localhost:5001/clone-e8ec7/us-central1/api'
+  // baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
 });
 
 export default instance;
